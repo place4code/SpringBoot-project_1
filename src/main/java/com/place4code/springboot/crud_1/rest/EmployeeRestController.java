73810dbc7e6042c4eddf43cb62ec30eb677f6c6c
place4code <place4code@gmail.com>
2019-04-13 18:32:19 +0200

    step 3: add rest controller

---
+++ src/main/java/com/place4code/springboot/crud_1/rest/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.place4code.springboot.crud_1.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.place4code.springboot.crud_1.dao.EmployeeDAO;
+import com.place4code.springboot.crud_1.entity.Employee;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+	
+	@Autowired
+	private EmployeeDAO employeeDAO;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAll() {
+		return employeeDAO.getAll();
+	}
+
+}
