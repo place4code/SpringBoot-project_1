a3a97e3e7de1f82fa202c2d2cea906bb62c28ea7
place4code <place4code@gmail.com>
2019-04-17 15:23:33 +0200

    last

--- src/main/java/com/place4code/springboot/crud_1/dao/EmployeeRepository.java
+++ src/main/java/com/place4code/springboot/crud_1/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.place4code.springboot.crud_1.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.place4code.springboot.crud_1.entity.Employee;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
